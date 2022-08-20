9797154152438788587b8924cedd8f5e5692c369
Yahya Yeşilyurt <95088352+yahyayslyrt03@users.noreply.github.com>
2022-08-20 22:33:45 +0300

    changes

--- db/db.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose");
-
-const dbUrl = "mongodb://localhost/chatapp";
-mongoose.connect(dbUrl)
-.then(() => console.log("Database connection successful"))
-.catch(error => console.log("Database error: "+ error.message));
